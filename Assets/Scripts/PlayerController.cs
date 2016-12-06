bbc7df739fd9fb0c494a0df0b3bbe7e146d2dc09
croush <claireroush@gmail.com>
2016-12-05 21:03:38 -0600

    Start
    OSX

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	//public AudioSource ping;
+	public AudioClip impact;
+	AudioSource audio;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		audio = GetComponent<AudioSource>();
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+		winText.text = "";
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement*speed);
+		if (Input.GetKey("escape"))
+			Application.Quit();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			countText.text = "Count: " + count.ToString ();
+			audio.PlayOneShot(impact);
+			if (count >= 12) {
+				winText.text = "You Win!";
+
+			}
+		}
+
+	}
+
+}
